fe888f233550b90a5f636072a0ac53d35e40d402
zvcwv <zvczvc111@gmail.com>
2026-01-22 23:12:19 +0500

    first commit

---
+++ ChatBot.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+
+client = OpenAI(api_key="")
+
+
+def chat_with_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+        
+    response = chat_with_gpt(user_input)
+    print("Chatbot: ", response)
+    
+    
